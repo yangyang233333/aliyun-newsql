363c4858751450fc6a269321f19056bbdfc10d18
qianyangyang <hfutqyy@163.com>
2022-10-01 22:15:30 +0800

    存档

--- commit.sh
+++ commit.sh
@@ -1,10 +1,10 @@
 # 提交代码到git，作为存档
 git remote add github git@github.com:yangyang233333/aliyun-newsql.git
-
+git remote add aliyun git@code.aliyun.com:1400996275/woaikv.git
 # 代码提交到阿里云code
 git add .
 git commit -m "存档"
-git push origin
+git push aliyun
 
 #代码提交到github
 git push github
