9d47836971f94e477181060271ee354d822dd382
qianyy <qianyy@localhost.localdomain>
2022-10-04 18:36:02 +0800

    存档

--- README.md
+++ README.md
@@ -1,1 +1,9 @@
 hello world.
+
+## 目录结构解释：
+1. src：项目的实现代码
+2. test2：用于测试src中的代码。运行test2下面的build.sh即可运行测试代码
+3. test：没啥用，可以任意修改。
+4. CMakeLists.txt：不可修改
+5. commit.sh：提交到远程仓库的脚本，主要作为备份
+6. build.sh：构建项目，不可修改
--- test2/test_main.cpp
+++ test2/test_main.cpp
@@ -11,17 +11,17 @@
 using namespace std;
 
 // 测试hmap的内存占用
-void test_hmap() {
-    unordered_map<int64_t, uint32_t> hmap;
-    int a = system("free -m");
-
-    for (size_t i = 0; i < 2*10000 * 10000; ++i) {
-        hmap.insert(make_pair(i,i+2));
-    }
-
-    a = system("free -m");
-
-}
+//void test_hmap() {
+//    unordered_map<int64_t, uint32_t> hmap;
+//    int a = system("free -m");
+//
+//    for (size_t i = 0; i < 2*10000 * 10000; ++i) {
+//        hmap.insert(make_pair(i,i+2));
+//    }
+//
+//    a = system("free -m");
+//
+//}
 
 // 测试以下corekv
 void test_corekv() {
@@ -42,7 +42,7 @@ int main() {
         auto b = iter->second;
     }
 
-    test_hmap();
+
 
 }
 
